c85fedd14bc0d34da4b3305f5f66b87822c6d621
Rotiahn <mail@rotiahn.com>
2023-12-03 11:51:30 -0500

    Kepler Constants

---
+++ kepler.gd
@@ -0,0 +1,60 @@
+class_name Kepler
+extends Node
+
+
+#  A library for handling orbital mechanics math and calculations
+# @author Rotiahn / https://github.com/Rotiahn/
+# @namespace kepler
+# 
+
+
+# signals
+
+# enums
+
+# constants
+const VERSION = '0.1.0'
+#const MAX_SAFE_LENGTH = Number.MAX_SAFE_INTEGER; # 9007199254740991 m ~= 0.95 light years
+
+#Constants - Distances
+const UNIT_LENGTH = 'meter'
+const KILOMETER = 1000
+const KM = KILOMETER
+const AU = 1.496e+11
+const LIGHT_SECOND = 2.998e+8
+const LIGHT_MINUTE = 1.799e+10
+const LIGHT_HOUR = 1.079e+12
+const LIGHT_DAY = 2.59e+13
+const LIGHT_YEAR = 9.461e+15
+
+#Constants - Time
+const UNIT_TIME = 'second'
+#const MAX_SAFE_TIME = Number.MAX_SAFE_INTEGER; # 9007199254740991 s ~= 285 Million years
+const MINUTE = 60
+const HOUR = 3600
+const DAY = 86400
+const YEAR = 3.154e+7
+
+#Constants - Rotation
+const UNIT_ROTATION = 'Radians'
+const DEGREE = 0.0174533
+#const PI = PI
+const DEGREES_PER_DAY = DEGREE / DAY
+
+#Constants - Mass
+const UNIT_mass = 'Kilograms'
+const TONNE = 1000
+const TON = TONNE
+const EARTH_MASS = 5.974e24
+const SOL_MASS = 1.9891e30
+
+#Constants - Physics
+const G = 6.674e-11 # Nm^2 / kg^2 = kg*m*(1/s^2)*m^2*(1/kg^2) = m^3/(kg*s^2)
+
+# @export variables
+
+# public variables
+
+# private variables
+
+# @onready variables
