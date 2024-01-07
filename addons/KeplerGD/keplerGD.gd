eb2395ebaedd8c3c9d7399986c3aaf6b191b3ca5
Rotiahn <mail@rotiahn.com>
2024-01-07 10:59:25 -0500

    Now it's a plugin!

---
+++ addons/KeplerGD/keplerGD.gd
@@ -0,0 +1,18 @@
+@tool
+extends EditorPlugin
+
+
+func _enter_tree() -> void:
+    # Initialization of the plugin goes here.
+    add_custom_type("Orbit Node", "Node", preload("orbit.gd"), preload("orbit_icon.png"))
+    add_custom_type("Orbit Mesh", "Node", preload("orbit.gd"), preload("orbit_mesh_icon.png"))
+    add_custom_type("AstroBody Node", "Node", preload("astro_body.gd"), preload("astro_body_icon.png"))
+    add_custom_type("Astro Mesh", "Node", preload("astro_mesh.gd"), preload("astro_mesh_icon.png"))
+    
+
+func _exit_tree() -> void:
+    # Clean-up of the plugin goes here.
+    remove_custom_type("Orbit Node")
+    remove_custom_type("Orbit Mesh")
+    remove_custom_type("AstroBody Node")
+    remove_custom_type("Astro Mesh")
